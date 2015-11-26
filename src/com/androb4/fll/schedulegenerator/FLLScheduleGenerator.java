4b886f94d02c1e2b085870fc614c561d71f982dd
Andres Abrego <andres.r.abrego@gmail.com>
2015-11-25 22:39:38 -0600

    Added code that deserializes a list of teams from an excel file

--- src/com/androb4/fll/schedulegenerator/FLLScheduleGenerator.java
+++ src/com/androb4/fll/schedulegenerator/FLLScheduleGenerator.java
@@ -5,41 +5,12 @@ import java.util.List;
 
 public class FLLScheduleGenerator {
 
-	private static int demo_teams[] = //{ 16, 118, 254, 441, 610, 624, 1114, 1484, 2587, 3847, 5892};
-	{1894,
-	1896,
-	1897,
-	2798,
-	3583,
-	6710,
-	8836,
-	8848,
-	11519,
-	13030,
-	13033,
-	14039,
-	15110,
-	17603,
-	17605,
-	18320,
-	18321,
-	19315,
-	19966,
-	20046,
-	20145,
-	20185,
-	20288,
-	20523,
-	21240,
-	21270,
-	21271,
-	21272,
-	22097,
-	22098,
-	22099}; /*,
-	22100
-};*/
-	
+	private static int demo_teams[] = // { 16, 118, 254, 441, 610, 624, 1114,
+										// 1484, 2587, 3847, 5892};
+	{ 1894, 1896, 1897, 2798, 3583, 6710, 8836, 8848, 11519, 13030, 13033, 14039, 15110, 17603, 17605, 18320, 18321,
+			19315, 19966, 20046, 20145, 20185, 20288, 20523, 21240, 21270, 21271, 21272, 22097, 22098,
+			22099}; //, 22100 };
+
 	/* Configs */
 	private static int kStartTime = 510; // 8:30 in minutes
 	private static int kNumMatchesEach = 3;
@@ -50,98 +21,102 @@ public class FLLScheduleGenerator {
 	private static int kPitTimeMin = 30;
 	private static int kMinTimeBetweenLast;
 	private static int kBreaks = 0;
-	
-	public static List<Team> teamList = new ArrayList<Team>();
+
+	public static Team[] teamList;
 	public static List<Match> matchList = new ArrayList<Match>();
-	
-	public static void main(String[] args) throws InterruptedException {
-		for(int team : demo_teams) {
-			teamList.add(new Team(team));
-		}
+
+	public static void main(String[] args) {
 		
-		while(!isEnoughMatches()) {
+		teamList = new Team[TeamListDeserializer.getTeams().length];
+		teamList = TeamListDeserializer.getTeams();
+
+		System.out.println(TeamListDeserializer.getTeams().length);
+		
+		while (!isEnoughMatches()) {
 			matchList.clear();
-			for(Team team3:teamList) {
+			for (Team team3 : teamList) {
 				team3.matches().clear();
 			}
 			int m = 1;
 			int t = kStartTime;
 			int kLastMatchTime = kStartTime - kMatchInterval;
 			Team[] teams = new Team[2];
-			
 			createSchedule:
-			while(!isEnoughMatches()) {
-				if(t == kLastMatchTime+kMatchInterval) {
-					
-					teams[0] = getRandomTeam();
-					long time = System.currentTimeMillis();
-					while(((t - teams[0].lastMatchTime()) < kPitTimeMin) || ((kNumMatchesEach - teams[0].matches().size()) <= 0)) {
-						teams[0] = getRandomTeam();
-						if(System.currentTimeMillis() - time > 50) {
+			while (!isEnoughMatches()) {
+				if (t == kLastMatchTime + kMatchInterval) {
+					for (int i = 0; i <teams.length; i++) {
+						teams[i] = getRandomTeam(teams);
+						long time = System.currentTimeMillis();
+						while (((t - teams[i].lastMatchTime()) < kPitTimeMin) || ((kNumMatchesEach - teams[i].matches().size()) <= 0)) {
+							teams[i] = getRandomTeam(teams);
+							if (System.currentTimeMillis() - time > 50) {
+								break createSchedule;
+							}
+						}
+						//System.out.println(isLastTeam());
+						if (isLastTeam() && teamList.length % 2 != 0) {
+							teams[1] = getRandomTeam(teams);
+							matchList.add(new Match(m, t, teams));
 							break createSchedule;
 						}
 					}
-					
-					if(isLastTeam() && teamList.size() % 2 != 0) {
-						teams[1] = getRandomTeam(teams[0]);
-						matchList.add(new Match(m, t, teams));
-						break createSchedule;
-					}
-					
-					teams[1] = getRandomTeam(teams[0]);
-					time = System.currentTimeMillis();
-					while((t - teams[1].lastMatchTime()) < kPitTimeMin || kNumMatchesEach - teams[1].matches().size() <= 0) {
-						teams[1] = getRandomTeam(teams[0]);
-						if(System.currentTimeMillis() - time > 50) {
-							break createSchedule;
-						}
-					}
+
 					matchList.add(new Match(m, t, teams));
 					kLastMatchTime = t;
 					m++;
 				}
 				t++;
 			}
-			//System.out.println(matchList.size());
+			System.out.println(matchList.size());
 		}
 		
 		// Print out schedule for debugging
-		for(Match match:matchList) {
-			System.out.print(match.matchNumber());
-			for(Team team:match.teams()) {
-				System.out.print(" " + team.teamNumber());
+		for (Match match : matchList) {
+			System.out.print(match.matchNumber() + ",");
+			for (Team team : match.teams()) {
+				System.out.print(" " + team.teamNumber() + ",");
 			}
 			System.out.print(" " + match.matchTime());
 			System.out.println();
 		}
 	}
-	
+
 	public static Team getRandomTeam() {
-		int randomNumber = (int)(Math.random() * teamList.size());
-		return teamList.get(randomNumber);
+		int randomNumber = (int) (Math.random() * teamList.length);
+		return teamList[randomNumber];
 	}
-	
-	public static Team getRandomTeam(Team excludeTeam) {
+
+	public static Team getRandomTeam(Team[] excludeTeams) {
 		Team team = getRandomTeam();
-		while(excludeTeam == team) {
-			team = getRandomTeam();
+		boolean done = true;
+		if(excludeTeams[0] == null) {
+			return team;
 		}
+		do {
+			team = getRandomTeam();
+			done = true;
+			for(Team excludeTeam:excludeTeams) {
+				if(excludeTeam != null && team == excludeTeam) {
+					done = false;
+				}
+			}
+		} while(!done);
 		return team;
 	}
-	
+
 	public static boolean isEnoughMatches() {
-		for(Team team:teamList) {
-			if((kNumMatchesEach - team.matches().size()) > 0) {
+		for (Team team : teamList) {
+			if ((kNumMatchesEach - team.matches().size()) > 0) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	public static boolean isLastTeam() {
 		int count = 0;
-		for(Team team:teamList) {
-			if(kNumMatchesEach - team.matches().size() > 0) {
+		for (Team team : teamList) {
+			if (kNumMatchesEach - team.matches().size() > 0) {
 				count++;
 			}
 		}
---
+++ src/com/androb4/fll/schedulegenerator/TeamListDeserializer.java
@@ -0,0 +1,73 @@
+package com.androb4.fll.schedulegenerator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TeamListDeserializer {
+	private static XSSFWorkbook teamListDocument;
+	private static XSSFSheet teamListSheet;
+	public static List<Team> teamList = new ArrayList<Team>();
+
+	private static void readTeamDocument() {
+		FileInputStream teamListFile;
+		try {
+			teamListFile = new FileInputStream(new File("2015-FLL-Hamilton-Team-List.xlsx"));
+			teamListDocument = new XSSFWorkbook(teamListFile);
+			teamListFile.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		teamListSheet = teamListDocument.getSheetAt(0);
+	}
+	
+	private static int getNumTeams() {
+		if (teamListSheet == null) {
+			readTeamDocument();
+		}
+		
+		int numTeams = 0;
+		for(int i=1; i<teamListSheet.getPhysicalNumberOfRows(); i++) {
+			if(teamListSheet.getRow(i).getCell(0).getNumericCellValue() != 0)
+				numTeams++;
+		}
+		return numTeams;
+	}
+
+	public static void deserializeTeams() {
+		if (teamListSheet == null) {
+			readTeamDocument();
+		}
+		
+		for(int i=1; i<=getNumTeams(); i++) {
+			Team team = new Team();
+			for(int c=0; c<2; c++) {
+				Cell cell = teamListSheet.getRow(i).getCell(c);
+				if(c==0)
+					team.setTeamNumber((int)cell.getNumericCellValue());
+				else if(c==1)
+					team.setTeamName(cell.getStringCellValue());
+				else if(c==2)
+					team.setTeamName(cell.getStringCellValue());
+			}
+			teamList.add(team);
+		}
+	}
+
+	public static Team[] getTeams() {
+		if (teamListSheet == null) {
+			deserializeTeams();
+		}
+		return teamList.toArray(new Team[teamList.size()]);
+	}
+
+}
