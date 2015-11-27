0b0888a91cb31f3c1cb0615ffc78dedb2a486bd4
Andres Abrego <andres.r.abrego@gmail.com>
2015-11-26 20:37:08 -0600

    Added code to format the output excel file. Added code to handle breaks.

--- src/com/androb4/fll/schedulegenerator/FLLScheduleGenerator.java
+++ src/com/androb4/fll/schedulegenerator/FLLScheduleGenerator.java
@@ -7,14 +7,14 @@ public class FLLScheduleGenerator {
 
 	/* Configs */
 	private static int kStartTime = 510; // 8:30 in minutes
-	private static int kNumMatchesEach = 3;
+	private static int kNumMatchesEach = 5;
 	private static int kNumFields = 6;
 	private static String kTableNames[] = {"Table 1", "Table 2", "Table 3", "Table 4", "Table 5", "Table 6"};
+	private static String eventName = "Hamilton MS FLL Tournament";
 	private static int kNumSets = 3;
 	private static int kMatchInterval = 5;
 	private static int kPitTimeMin = 5;
-	private static int kMinTimeBetweenLast;
-	private static int kBreaks = 0;
+	private static int[][] breaks = new int[][]{{720, 30}};
 
 	public static Team[] teamList;
 	public static Table[] tableList = new Table[kTableNames.length];
@@ -40,7 +40,7 @@ public class FLLScheduleGenerator {
 			Team[] teams = new Team[2];
 			createSchedule:
 			while (!isEnoughMatches()) {
-				if (t == kLastMatchTime + kMatchInterval) {
+				if (t >= kLastMatchTime + kMatchInterval && !isBreak(t)) {
 					for (int i = 0; i <teams.length; i++) {
 						teams[i] = getRandomTeam(teams);
 						long time = System.currentTimeMillis();
@@ -53,18 +53,16 @@ public class FLLScheduleGenerator {
 						//System.out.println(isLastTeam());
 						if (isLastTeam() && teamList.length % 2 != 0) {
 							teams[1] = getRandomTeam(teams);
-							matchList.add(new Match(m, t, teams, new Table[]{tableList[0], tableList[1]}));
+							matchList.add(new Match(m, t, teams, new Table[]{tableList[3*((m-1)%3)-((m-1)%3)], tableList[2*((m-1)%3)+1]}));
 							break createSchedule;
 						}
 					}
-
 					matchList.add(new Match(m, t, teams, new Table[]{tableList[3*((m-1)%3)-((m-1)%3)], tableList[2*((m-1)%3)+1]}));
 					kLastMatchTime = t;
 					m++;
 				}
 				t++;
 			}
-			System.out.println(matchList.size());
 		}
 		
 		// Print out schedule for debugging
@@ -73,11 +71,11 @@ public class FLLScheduleGenerator {
 			for(int t=0; t<match.teams().length; t++) {
 				System.out.print(match.tables()[t].getName() + " " + match.teams()[t].teamNumber() + ", ");
 			}
-			System.out.print(match.matchTime());
+			System.out.print(Util.minutesToTimeString(match.matchTime()));
 			System.out.println();
 		}
 		
-		MatchScheduleSerializer.serializeSchedule(matchList.toArray(new Match[matchList.size()]), tableList);
+		MatchScheduleSerializer.serializeSchedule(matchList.toArray(new Match[matchList.size()]), tableList, eventName);
 	}
 
 	public static Team getRandomTeam() {
@@ -120,5 +118,14 @@ public class FLLScheduleGenerator {
 			}
 		}
 		return count<2;
+	}
+	
+	public static boolean isBreak(int time) {
+		for(int[] b:breaks) {
+			if(time >= b[0] && time < b[0]+b[1]) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- src/com/androb4/fll/schedulegenerator/MatchScheduleSerializer.java
+++ src/com/androb4/fll/schedulegenerator/MatchScheduleSerializer.java
@@ -2,11 +2,16 @@ package com.androb4.fll.schedulegenerator;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import org.apache.poi.hssf.usermodel.HeaderFooter;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Footer;
 import org.apache.poi.ss.usermodel.PrintSetup;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -14,41 +19,62 @@ public class MatchScheduleSerializer {
 	private static XSSFWorkbook matchScheduleDocument;
 	private static XSSFSheet matchScheduleSheet;
 	private static CellStyle style2;
+	private static Match[] matchList;
 	private static Table[] tableList;
+	private static String eventName;
 	
 	private static void createTitle() {
-		style2.setAlignment(CellStyle.ALIGN_CENTER);
-	    style2.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
-	    
+		CellStyle titleStyle = matchScheduleDocument.createCellStyle();
+		titleStyle.setAlignment(CellStyle.ALIGN_CENTER);
+		titleStyle.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		titleStyle.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
+		titleStyle.setFillForegroundColor(HSSFColor.TURQUOISE.index);
+		titleStyle.setBorderBottom(CellStyle.BORDER_THIN);
+		titleStyle.setBorderTop(CellStyle.BORDER_THIN);
+		titleStyle.setBorderLeft(CellStyle.BORDER_THIN);
+		titleStyle.setBorderRight(CellStyle.BORDER_THIN);
+		titleStyle.setFont(getTitleFont());
+		
+		Row row = matchScheduleSheet.createRow(0);
+		Cell cell;
+		for(int c=0; c<=7; c++) {
+			cell = row.createCell(c);
+			cell.setCellValue(eventName + " - Match Schedule");
+			cell.setCellStyle(titleStyle);
+		}
 		matchScheduleSheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 7));
-		Cell cell = matchScheduleSheet.createRow(0).createCell(0);
-		cell.setCellValue("Event Name");
-		cell.setCellStyle(style2);
-		Row row = matchScheduleSheet.createRow(1);
+		
+		row = matchScheduleSheet.createRow(1);
 		cell = row.createCell(0);
 		cell.setCellValue("Match");
-		cell.setCellStyle(style2);
+		cell.setCellStyle(titleStyle);
 		cell = row.createCell(1);
 		cell.setCellValue("Match Time");
-		cell.setCellStyle(style2);
+		cell.setCellStyle(titleStyle);
 		
 		for(int t=0; t<tableList.length; t++) {
 			cell = row.createCell(t+2);
 			cell.setCellValue(tableList[t].getName());
-			cell.setCellStyle(style2);
+			cell.setCellStyle(titleStyle);
 		}
 		matchScheduleSheet.setRepeatingRows(new CellRangeAddress(0, 1, 0, 0));
 	}
 	
-	public static void serializeSchedule(Match[] matches, Table[] tableList) {
-		
+	public static void serializeSchedule(Match[] matches, Table[] tableList, String eventName) {
 		MatchScheduleSerializer.tableList = tableList;
-		
+		matchList = matches;
+		MatchScheduleSerializer.eventName = eventName;
 		//Blank workbook
         matchScheduleDocument = new XSSFWorkbook();
         style2 = matchScheduleDocument.createCellStyle();
-        		
-        //Create a blank sheet
+        style2.setAlignment(CellStyle.ALIGN_CENTER);
+        style2.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+        style2.setBorderBottom(CellStyle.BORDER_THIN);
+        style2.setBorderTop(CellStyle.BORDER_THIN);
+        style2.setBorderLeft(CellStyle.BORDER_THIN);
+        style2.setBorderRight(CellStyle.BORDER_THIN);
+        style2.setFont(getDefaultFont());
+        
         matchScheduleSheet = matchScheduleDocument.createSheet("Match Schedule");
         
         createTitle();
@@ -59,19 +85,24 @@ public class MatchScheduleSerializer {
             cell.setCellValue(matches[m].matchNumber());
             cell.setCellStyle(style2);
             cell = row.createCell(1);
-            cell.setCellValue(matches[m].matchTime());
+            cell.setCellValue(Util.minutesToTimeString(matches[m].matchTime()));
             cell.setCellStyle(style2);
             
-            for(int i=0; i<matches[i].teams().length; i++) {
-            	cell = row.createCell(matches[m].tables()[i].getNumber()+1);
-                cell.setCellValue(matches[m].teams()[i].teamNumber());
-                cell.setCellStyle(style2);
-            }
+            for(int c=2; c<=tableList.length+1; c++) {
+    			cell = row.createCell(c);
+    			if(c-1==matchList[m].tables()[0].getNumber()) {
+    				cell.setCellValue(matchList[m].teams()[0].teamNumber());
+    			}
+    			if(c-1==matchList[m].tables()[1].getNumber()) {
+    				cell.setCellValue(matchList[m].teams()[1].teamNumber());
+    			}
+    			cell.setCellStyle(style2);
+    		}
         }
         
         autoSizeColumns();
-        
         matchScheduleSheet.setAutobreaks(true);
+        setFooter();
         
         PrintSetup printSetup = matchScheduleSheet.getPrintSetup();
         printSetup.setFitHeight((short)0);
@@ -93,7 +124,29 @@ public class MatchScheduleSerializer {
 	
 	private static void autoSizeColumns() {
 		for(int i=0; i<=7; i++) {
-			matchScheduleSheet.setDefaultColumnWidth(10);;
+			matchScheduleSheet.setDefaultColumnWidth(10);
 		}
+	}
+	
+	private static void setFooter() {
+		Footer footer = matchScheduleSheet.getFooter();
+	    footer.setCenter("Page " + HeaderFooter.page() + " of " + HeaderFooter.numPages());
+	}
+	
+	private static XSSFFont getTitleFont() {
+		XSSFFont font;
+		font = matchScheduleDocument.createFont();
+		font.setFontHeightInPoints((short)12);
+		font.setFontName("Calibri");
+		font.setBold(true);
+	    return font;
+	}
+	
+	private static XSSFFont getDefaultFont() {
+		XSSFFont font;
+		font = matchScheduleDocument.createFont();
+		font.setFontHeightInPoints((short)12);
+		font.setFontName("Calibri");
+	    return font;
 	}
 }
